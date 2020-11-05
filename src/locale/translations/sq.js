97c9d10242b593056924fcc165059bdc7e8d7a39
Marian Heinsen <mail@marianheinsen.com>
2020-11-05 13:47:05 +0100

    remove all languages except de and en

--- src/locale/translations/sq.js
+++
@@ -1,8 +0,0 @@
-import Language from '../Language'
-
-export default new Language(
-  'Albanian',
-  ['Janar', 'Shkurt', 'Mars', 'Prill', 'Maj', 'Qershor', 'Korrik', 'Gusht', 'Shtator', 'Tetor', 'Nëntor', 'Dhjetor'],
-  ['Jan', 'Shk', 'Mar', 'Pri', 'Maj', 'Qer', 'Korr', 'Gus', 'Sht', 'Tet', 'Nën', 'Dhj'],
-  ['D', 'Ha', 'Ma', 'Mr', 'Ej', 'Pr', 'Sh'],
-)
